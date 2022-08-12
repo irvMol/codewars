68cf79387ee9f1946337d3958086f6399827372a
irving <asteroid@pop-os.localdomain>
2022-08-12 12:45:06 -0600

    boolToWord added

--- 8kyu/boolToString.js
+++ 8kyu/boolToString.js
@@ -1,15 +1,12 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 function boolToWord( bool ){
- var word
+
    
  if (!bool)
  {
-    word = "no"
+   return "no"
  }
 
- else word = "yes"
-console.log(word) 
+ else return "yes"
 }
-
-boolToWord(false)
